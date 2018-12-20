1043672aedc5168911f95e73410c4bb46aa38b14
batman <arik.nasatissin@panaya.com>
2018-12-20 16:23:15 +0200

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
@@ -25,6 +26,22 @@ app.get('/todos', (req,res)=>{
         res.send({todos});
     }, (err)=>{
         res.status(400).send(e);
+    });
+});
+
+app.get('/todos/:id', (req,res)=>{
+    let todoId = req.params.id;
+    if (!ObjectID.isValid(todoId)){
+        return res.status(404).send();
+    }
+
+    Todo.findById(todoId).then((todo)=>{
+        if (!todo){
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e)=>{
+        return res.status(400).send();
     });
 });
 
